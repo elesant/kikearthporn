d908790472c5b159603dd1eb5485fd6c7492cacd
kmcgaire <kevinmcgaire@gmail.com>
2014-03-31 11:41:34 -0400

    Dat Rework

--- web/js/main.js
+++ web/js/main.js
@@ -1,4 +1,7 @@
 (function (App) {
+  if (kik.browser) {
+    kik.browser.statusBar(true)
+  }
 	try {
 			App.restore();
 		} catch (err) {
--- web/js/page-home.js
+++ web/js/page-home.js
@@ -1,38 +1,54 @@
 App.populator('home', function (page) {
   var urls = [];
-  var index = 0;
 
-  $.getJSON('http://reddit.com/r/EarthPorn/.json?jsonp=?', function (data) {
-    $.each(data.data.children, function (i, item) {
-      var image = item.data.url;
-      var thumbnail = item.data.thumbnail;
-      if ((i != 0 || (image.indexOf('imgur') == -1 || image.indexOf('.jpg') == -1)) && (image.indexOf("comment") == -1)) {
-        $('<img/>').attr('class', 'thumbnail').attr('src', thumbnail).attr('id', 'image-' + index).appendTo('#images');
-        if (image.indexOf('imgur') == -1) {
+  getData();
+
+  function getData() {
+    var index = 0;
+    $.getJSON('http://reddit.com/r/EarthPorn/.json?jsonp=?', function (data) {
+      // First items returned in data isn't useful
+      var r = $.Deferred();
+      for (var i = 1; i < data.data.children.length; i++) {
+        var item = data.data.children[i];
+        var image = item.data.url;
+        var thumbnail = item.data.thumbnail;
+
+        if (image.indexOf('.jpg') != -1) {
+          renderThumbnail(thumbnail, index);
           urls.push(image);
+          console.log('Pushed: ' + image + ' onto urls');
           index++;
-          console.log('Pushing: ' + item.data.url + ' onto urls');
-        } else {
-          var newurl = image.replace('imgur', 'i.imgur') + '.jpg';
-          urls.push(newurl);
+        } else if (image.indexOf('imgur') != -1) {
+          renderThumbnail(thumbnail, index);
+          urls.push(image.replace('imgur','i.imgur') + '.jpg');
+          console.log('Pushed: ' + image.replace('imgur','i.imgur') + '.jpg' + ' onto urls');
           index++;
-          console.log('Pushing: ' + newurl + ' onto urls');
         }
-
-        var imageSelector = (page).querySelector('#image-' + (i - 1).toString());
-        imageSelector.addEventListener('click', function () {
-          console.log('URL full image is ' + urls[i]);
-          App.load('viewer', {
-            image: urls[index],
-            index: index,
-            images: urls
-          });
-        })
-
       }
+      return r.done(addSelectors());
     });
+  }
 
-  });
+
+  function renderThumbnail(thumbnail, index){
+    $('<img/>').attr('class', 'thumbnail').attr('src', thumbnail).attr('id', 'image-' + index).appendTo('#images');
+  }
+
+
+  function addSelectors() {
+    for (var i = 0; i < urls.length; i++) {
+      var imageSelector = (page).querySelector('#image-' + i);
+      imageSelector.addEventListener('click', function () {
+        console.log('URL full image is ' + urls[i]);
+        App.load('viewer', {
+          image: urls[i],
+          index: i,
+          images: urls
+        });
+      })
+    }
+  }
 
 
 });
+
