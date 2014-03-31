7d7010fba96add364baf01cf177d5d4fa1b59e60
kmcgaire <kevinmcgaire@gmail.com>
2014-03-31 18:25:03 -0400

    Kik to share - need to make a page for when you open a message

--- web/js/page-viewer.js
+++ web/js/page-viewer.js
@@ -2,12 +2,12 @@
  * Created by kevin on 3/31/2014.
  */
 
-App.populator('viewer', function(page, data){
+App.populator('viewer', function (page, data) {
 
   var photoViewer = new PhotoViewer(page, data.images, {
-    automaticTitles : false ,
-    autoHideTitle   : true ,
-    startAt         : (data.index || 0)
+    automaticTitles: false,
+    autoHideTitle: true,
+    startAt: (data.index || 0)
   });
 
   if (data.images) {
@@ -16,4 +16,17 @@ App.populator('viewer', function(page, data){
       App.saveStack();
     });
   }
+  page.querySelector('#share').addEventListener('click', function () {
+    var image = data.images ? data.images[data.index] : data.image;
+    if (kik.send) {
+      kik.send({
+        title: 'Earth Porn',
+        text: 'Like Youve Never seen her before',
+        pic: image, // optional
+        big: true, // optional
+        noForward: true, // optional
+        data: { some: 'json' }         // optional
+      });
+    }
+  });
 });
